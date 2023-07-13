f590b5e83e88318851f23a0907831e84cad9e9f0
jcg0 <jg.awsprod@gmail.com>
2023-07-12 22:14:58 -0400

    created  the rest of the page files needed for the portfolio. next step is to add conditional rendering to display the correct pages when a user clicks on one of the nav elements

--- src/components/NavItems.js
+++ src/components/NavItems.js
@@ -1,3 +1,5 @@
+import "./NavItems.css";
+
 const NavItems = () => {
   return (
     <div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,11 +1,19 @@
 import Home from "./Home";
 import NavBar from "../components/NavBar";
+import About from "./About";
+import Projects from "./Projects";
+import Skills from "./Skills";
+import Contact from "./Contact";
 
 const Portfolio = () => {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
     </div>
   );
 };
